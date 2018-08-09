ea20c9622aea09bad5b7f19ef5457a8b91631b0d
Charlotte Skinner <cs.skinner90@hotmail.co.uk>
2018-08-09 12:53:11 +0000

    Refactor views to use HTML templates and added handling of the user form

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, redirect
+from datetime import datetime
+from flask import Flask, redirect, render_template, request
 
 
 app = Flask(__name__)
@@ -7,22 +8,29 @@ messages = []
 
 def add_messages(username, message):
     """ Add messages to the `messages` list """
-    messages.append("{}: {}".format(username, message))
+    now = datetime.now().strftime("%H:%M:%S")
+    message_dict = {"timestamp": now, "from": username, "message": message}
+    messages.append(message_dict)
     
 def get_all_messages():
     """ Get all of messages and separate using a br """
-    return "<br>".join(messages)
+    return messages
     
     
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
     """ Main page with instructions """
-    return "To send a message use /USERNAME/MESSAGE"
+    if request.method == "POST":
+        with open("data/users.txt", "a") as user_list:
+            user_list.writelines(request.form["username"] + "\n")
+        return redirect(request.form["username"])
+    return render_template("index.html")
     
 @app.route('/<username>')
 def user(username):
     """Display chat messages"""
-    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
+    messages = get_all_messages()
+    return render_template("chat.html", username=username, chat_messages=messages)
     
 @app.route('/<username>/<message>')
 def send_message(username, message):
